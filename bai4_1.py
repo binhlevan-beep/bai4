079a16eea1c5023f49518297acf2985361ee77d6
binhlevan-beep <binh2909.hihi@gmail.com>
2025-04-03 15:52:05 +0700

    Add files via upload

---
+++ bai4_1.py
@@ -0,0 +1,5 @@
+S = input('nhap chuoi:')
+for ch in S:
+    print(ch)
+print("sinh vien: Lê Văn Bình")
+print("Mssv: 235752020710002")
---
+++ bai4_2.py
@@ -0,0 +1,7 @@
+S = input('Nhap chuoi: ')
+for ch in S:
+    if ch not in (' ', '\t'):
+        print(ch)
+print("sinh vien: Lê Văn Bình")
+")
+print("Mssv: 2357520207100022")
